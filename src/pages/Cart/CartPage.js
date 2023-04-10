62480f2fa498cf3cf25923834e675baf3e831a3b
Abby Alexander911@gmail.com <abbyalexander911@gmail.com>
2023-04-09 22:49:14 -0400

    codeBook

---
+++ src/pages/Cart/CartPage.js
@@ -0,0 +1,16 @@
+import { CartEmpty } from "./components/CartEmpty";
+import { CartList } from "./components/CartList";
+import { useCart } from "../../context";
+import { useTitle } from "../../hooks/useTitle";
+
+export const CartPage = () => {
+  // const cartlistLength = 0;
+  const { cartList } = useCart();
+  useTitle(`Cart (${cartList.length})`);
+
+  return (
+    <main>
+      { cartList.length ? <CartList /> : <CartEmpty /> }
+    </main>
+  )
+}
